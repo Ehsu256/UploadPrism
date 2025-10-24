cefc90b124da459b1ccb6ce23c14e4e6e9a792c6
Ehsu256 <estic255@gmail.com>
2025-10-24 15:16:57 -0500

    Add a way to get user UID with given server info and add createContextMenu()

--- popup.js
+++ popup.js
@@ -1,20 +1,41 @@
 // GETTING USER'S SERVER INFO FROM FORM
-const serverInfoForm = document.getElementById("serverInfoForm");
-serverInfoForm.addEventListener("submit", getServerInfo);
+const serverInfoForm = document.getElementById('serverInfoForm');
 
-function getServerInfo() {
-  const photoprismUrl = document.getElementById("photoprismUrl").value;
-  const authToken = document.getElementById("authToken").value;
-  const userUid = document.getElementById("userUid").value;
-
+serverInfoForm.addEventListener('submit', async () => {
+  const photoprismUrl = document.getElementById('photoprismUrl').value;
+  const authToken = document.getElementById('authToken').value;
   const serverInfo = {
     photoprismUrl: photoprismUrl,
-    authToken: authToken,
-    userUid: userUid
+    authToken: authToken
   };
 
+  // Getting user UID
+  const response = await fetch(photoprismUrl+'/api/v1/session', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + authToken
+    }
+  });
+  const session = await response.json();
+  const userUid = session.user.UID;
+  serverInfo["userUid"] = userUid;
+
+  // Saving info to local storage
   chrome.storage.local.set({ serverInfo: serverInfo })
-  .then(() => {
-    console.log("Server info saved:", serverInfo);
+    .then(createContextMenus);
+});
+
+
+// CREATE CONTEXT MENUS
+async function createContextMenus() {
+  chrome.contextMenus.create({
+    title: 'media',
+    contexts: ['image', 'video', 'audio'],
+    id: 'media'
+  },
+  function () {
+    if (chrome.runtime.lastError) {
+      console.log('Got error when creating context menu: ' + chrome.runtime.lastError.message);
+    }
   });
 }
