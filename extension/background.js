0f70b488f29cf6305d78a88b099c928e2c3f53d9
Ehsu256 <estic255@gmail.com>
2025-10-26 19:57:46 -0500

    Icons change according to upload status
    Added new icons that change according to upload status, moved file upload process to its own function, minor changes in some variable declarations.

--- extension/background.js
+++ extension/background.js
@@ -1,48 +1,95 @@
-// UPLOAD AND IMPORT FILE TO PHOTOPRISM ON CONTEXT MENU ITEM CLICK
+// PREPARING FILE FOR UPLOAD
 chrome.contextMenus.onClicked.addListener(async (info) => {
-  // RETRIEVING USER'S SERVER INFO FROM STORAGE
-  // Retrieving it before every upload may be inefficient
-  const serverInfo = await chrome.storage.local.get(['serverInfo']);
-  const photoprismUrl = serverInfo.serverInfo.photoprismUrl;
-  const authToken = serverInfo.serverInfo.authToken;
-  const userUid = serverInfo.serverInfo.userUid;
+  setIcon('processing');
 
-  // PREPARING FILE FOR UPLOAD
   // Fetching the right-clicked file
   const fileUrl = info.srcUrl;
   const response = await fetch(fileUrl);
   const blob = await response.blob();
 
   // Naming the file with the correct extension to prevent "unsupported file extension" error in PhotoPrism
-  const extension = blob.type.split('/')[1]; // Getting the file extension from MIME type
-  const fileName = 'UploadPrism.' + extension;
+  const fileExtension = blob.type.split('/')[1]; // Getting the file extension from MIME type
+  const fileName = 'UploadPrism.' + fileExtension;
   const fileObject = new File([blob], fileName, { type: blob.type });
 
-  // Creating FormData and appending the file object
+  // Creating FormData and appending the File object
   const formData = new FormData();
   formData.append('files', fileObject);
-  console.log('FILE INFORMATION:\n', formData.get('files'));
 
-  // UPLOADING THE FILE
-  const uploadResponse = await fetch(photoprismUrl+'/api/v1/users/'+userUid+'/upload/_UploadPrism', {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer ' + authToken,
-      'Connection': 'keep-alive'
-    },
-    body: formData
-  });
-  console.log('UPLOAD RESULTS:\n', await uploadResponse.json());
-
-  // IMPORTING THE FILE
-  const importResponse = await fetch(photoprismUrl+'/api/v1/users/'+userUid+'/upload/_UploadPrism', {
-    method: 'PUT',
-    headers: {
-      'Authorization': 'Bearer ' + authToken,
-      'Connection': 'keep-alive',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ albums: [] })
-  });
-  console.log('IMPORT RESULTS:\n', await importResponse.json());
+  uploadFile(formData);
 });
+
+
+// UPOLOADING FILE
+async function uploadFile(formData) {
+  // Retrieving server info from local storage
+  const serverInfo = await chrome.storage.local.get(['serverInfo']);
+  const { photoprismUrl, authToken, userUid } = serverInfo.serverInfo; // Object destructuring
+
+  try {
+    // Uploading image with POST
+    const uploadResponse = await fetch(photoprismUrl+'/api/v1/users/'+userUid+'/upload/_UploadPrism', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + authToken,
+        'Connection': 'keep-alive'
+      },
+      body: formData
+    });
+
+    // Importing image with PUT
+    const importResponse = await fetch(photoprismUrl+'/api/v1/users/'+userUid+'/upload/_UploadPrism', {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + authToken,
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ albums: [] })
+    });
+
+    if ((await uploadResponse.json()).code == 200 && (await importResponse.json()).code == 200) {
+      setIcon('success');
+    }
+  } catch (error) {
+    console.log("Error when trying to upload/import media", error);
+    setIcon('error');
+  }
+}
+
+
+// ICON CHANGES - processing, success, error
+function setIcon(uploadStatus) {
+  switch (uploadStatus) {
+    case 'processing':
+      chrome.action.setIcon({
+        path: "icons/icon128-processing.png"
+      });
+      break;
+    case 'success':
+      chrome.action.setIcon({
+        path: "icons/icon128-success.png"
+      });
+      setToDefault();
+      break;
+    case 'error':
+      chrome.action.setIcon({
+        path: "icons/icon128-error.png"
+      });
+      setToDefault();
+      break;
+  
+    default:
+      console.log("Unknown case, setting icon to default");
+      setToDefault();
+      break;
+  }
+
+  function setToDefault() {
+    setTimeout(() => {
+      chrome.action.setIcon({
+        path: "icons/icon128.png"
+      })
+    }, 3000);
+  }
+}
